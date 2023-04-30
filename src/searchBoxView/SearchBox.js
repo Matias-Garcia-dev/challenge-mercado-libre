5494babf27c82243a23d6c5e67203cf574f69fc5
Matias <matiasenriquegarciadev@gmail.com>
2023-04-30 03:05:03 -0300

    header

---
+++ src/searchBoxView/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/common/Header'
+
+const SearchBox = () => {
+  return (
+    <div><Header></Header></div>
+  )
+}
+
+export default SearchBox
